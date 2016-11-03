bbc7be95a09179049386fbc0ca69b547701d353f
jquesadaabeijon <jquesadaabeijon@atanasoff32.danielcastelao.org>
2016-11-03 09:25:07 +0100

    0.1

---
+++ src/branchcontornos/BranchContornos.java
@@ -0,0 +1,12 @@
+
+package branchcontornos;
+
+public class BranchContornos {
+
+    public static void main(String[] args) {
+        ClassContornos cl = new ClassContornos();
+        cl.calcularArea();
+        
+    }
+    
+}
