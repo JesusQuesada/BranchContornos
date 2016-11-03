f63f55112b5f7b59cdb2110dfcee5737ae4ea321
jquesadaabeijon <jquesadaabeijon@atanasoff32.danielcastelao.org>
2016-11-03 09:26:30 +0100

    0.2

--- src/branchcontornos/ClassContornos.java
+++ src/branchcontornos/ClassContornos.java
@@ -10,18 +10,18 @@ public class ClassContornos {
         switch (opcion) {
             case 1: 
                 float lado = Float.parseFloat(JOptionPane.showInputDialog("Introducir lado "));
-                    System.out.println(lado*lado);
+                    //System.out.println(lado*lado);
                 break;
             case 2: 
                 float base = Float.parseFloat(JOptionPane.showInputDialog("Introducir base "));
                 float altura = Integer.parseInt(JOptionPane.showInputDialog("Introducir altura "));
-                    System.out.println(base*altura/2);
+                    //System.out.println(base*altura/2);
                 break;
             case 3: 
                 float radio = Float.parseFloat(JOptionPane.showInputDialog("Introducir radio "));
-                    System.out.println(Math.PI*Math.pow(radio, 2));
+                    //System.out.println(Math.PI*Math.pow(radio, 2));
                 break;
-            default: System.out.println("Opción incorrecta ");
+            default: //System.out.println("Opción incorrecta ");
         }
     }
 }
